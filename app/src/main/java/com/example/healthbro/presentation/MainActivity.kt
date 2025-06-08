8643bed426e4fd97873ec94d657840e3f35cfb03
aibelbin <aibelbinzacariah@gmail.com>
2025-06-08 14:19:48 +0530

    [FEAT] Transssiitionnnnn

--- app/src/main/java/com/example/healthbro/presentation/MainActivity.kt
+++ app/src/main/java/com/example/healthbro/presentation/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.healthbro.presentation
 
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -19,18 +28,45 @@ import com.example.healthbro.presentation.theme.FinanceTrackerTheme
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setWindowAnimations(0)  // Disable ALL system window animations
+        //window.setBackgroundDrawable(ColorDrawable(Color.Transparent))
         setContent {
             FinanceTrackerTheme {
                 val navController = rememberNavController()
 
                 NavHost(
                     navController = navController,
-                    startDestination = "MainScreen"
+                    startDestination = "MainScreen",
+                    modifier = Modifier.fillMaxSize()
                 ){
-                    composable("SetupScreen") { SetupScreen(navController)  }
-                    composable("MainScreen") { MainScreen(navController = navController) }
+
+                    composable(
+                        route = "MainScreen",
+                        exitTransition = {
+                            slideOutOfContainer(towards = AnimatedContentTransitionScope.SlideDirection.Left)
+                        },
+                        enterTransition = {
+                            slideIntoContainer(towards = AnimatedContentTransitionScope.SlideDirection.Right)
+                        }
+                    ) {
+                        MainScreen(navController)
+                    }
+
+
+                    composable(
+                        route = "SetupScreen",
+                        enterTransition = {
+                            slideIntoContainer(towards = AnimatedContentTransitionScope.SlideDirection.Left)
+                        },
+                        exitTransition = {
+                            slideOutOfContainer(towards = AnimatedContentTransitionScope.SlideDirection.Right)  
+                        }
+                    ) {
+                        SetupScreen(navController)
+                    }
                 }
                 }
             }
